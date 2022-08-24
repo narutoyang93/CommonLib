0849391653570b925ec752d3a4c57d77c38a18bd
杨晓城 <yangxiaocheng@zhilingsd.com>
2022-08-24 11:49:04 +0800

    完善LifecycleUtil

--- CommonLib/src/main/java/com/naruto/lib/common/utils/LifecycleUtil.kt
+++ CommonLib/src/main/java/com/naruto/lib/common/utils/LifecycleUtil.kt
@@ -15,22 +15,17 @@ import java.lang.ref.WeakReference
 object LifecycleUtil {
     /**
      * 添加监听
-     *
-     * @param lifecycleOwner
-     * @param targetObject
-     * @param lifecycleEvent
-     * @param operation
-     * @param <T>
-     * @return 用于移除监听
-    </T> */
-    fun <T> addObserver(
-        lifecycleOwner: LifecycleOwner, targetObject: T,
-        lifecycleEvent: Lifecycle.Event, operation: T.() -> Unit
+     * @param lifecycleOwner LifecycleOwner
+     * @param lifecycleEvent Event
+     * @param callback Function0<Unit>
+     * @return () -> Unit 用于移除监听
+     */
+    fun addObserver(
+        lifecycleOwner: LifecycleOwner, lifecycleEvent: Lifecycle.Event, callback: () -> Unit
     ): () -> Unit {
-        val wf = WeakReference(targetObject)
         val lifecycleObserver: LifecycleObserver =
             LifecycleEventObserver { _: LifecycleOwner?, event: Lifecycle.Event ->
-                if (event == lifecycleEvent) wf.get()?.operation()
+                if (event == lifecycleEvent) callback()
             }
         val lifecycle = lifecycleOwner.lifecycle.apply { addObserver(lifecycleObserver) }
         val wf_lifecycle = WeakReference(lifecycle)
@@ -42,15 +37,33 @@ object LifecycleUtil {
     }
 
     /**
+     * 添加监听(防止内存泄漏)
+     *
+     * @param lifecycleOwner
+     * @param targetObject 回调时要处理的对象
+     * @param lifecycleEvent
+     * @param callback
+     * @param <T>
+     * @return 用于移除监听
+    </T> */
+    fun <T> addObserver(
+        lifecycleOwner: LifecycleOwner, targetObject: T,
+        lifecycleEvent: Lifecycle.Event, callback: T.() -> Unit
+    ): () -> Unit {
+        val wf = WeakReference(targetObject)
+        return addObserver(lifecycleOwner, lifecycleEvent) { wf.get()?.callback() }
+    }
+
+    /**
      * 添加 ON_DESTROY 监听
      *
      * @param lifecycleOwner
      * @param targetObject
-     * @param operation
+     * @param callback
      * @param <T>
      * @return 用于移除监听
     </T> */
     fun <T> addDestroyObserver(
-        lifecycleOwner: LifecycleOwner, targetObject: T, operation: T.() -> Unit
-    ) = addObserver(lifecycleOwner, targetObject, Lifecycle.Event.ON_DESTROY, operation)
+        lifecycleOwner: LifecycleOwner, targetObject: T, callback: T.() -> Unit
+    ) = addObserver(lifecycleOwner, targetObject, Lifecycle.Event.ON_DESTROY, callback)
 }
